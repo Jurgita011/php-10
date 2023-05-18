ba34fd16ca8224f4ed6e1c40b0031b747725818d
Jurgita <jurgita011@gmail.com>
2023-05-18 18:28:39 +0300

    php

---
+++ 010/u.php
@@ -0,0 +1,17 @@
+<?php
+
+ echo '<pre>';
+echo'______1u___<br>';
+
+//echo "\n\n\n___________1U_______________\n"; 
+$petras = rand(10, 20);
+$jonas = rand(5, 25);
+
+if ($petras > $jonas) {
+    echo "Petas: $petras, Jonas: $jonas, laimėjo Petras";
+} elseif ($petras < $jonas) {
+    echo "Petas: $petras, Jonas: $jonas, laimėjo Jonas";
+} else {
+    echo "Petas: $petras, Jonas: $jonas, niekas nelaimėjo";
+}
+
