e84c507a29c53578b2dba16ba412a8e988e635c1
Jurgita <jurgita011@gmail.com>
2023-06-12 12:02:57 +0300

    object

--- 022/Ufo.php
+++ 022/Ufo.php
@@ -1,22 +1,88 @@
 <?php
-// Petras
 
-class Ufo {
+// Objektinis programavimas: vienai klasei - vienas failas,  klases vardas is didziosios, failo pavadinimas kaip klases pavadinimas
 
-    public $speed = 5;
+// Objekto viduje - kintamasis, objekto savybe. Funkcija - klases kontekste vadinama metodu.
+
+// Deklaruojant kintamaji, reikia pasakyti, kokio matomumo (lygio) jis yra: public, protected ar private.
+// Funkcijos turi ta pati matomuma kaip ir kintamieji.
+
+// public - visiems laisvai prieinama (pvz., dezute guli namo kieme) - - Rodo kieme (index.php)
+// protected - prieinama tik tam tikram ratui (pvz., dezute yra namo bute)
+// private - prieinama tik su leidimu (pvz., dezute guli seife, kuri gali atrakinti tik su raktu) - Nerodo kieme (index.php)-Fatal error
+
+// Private ir protected savybems nuskaityti naudojami magic metodai - leidzia nuskaityti, bet nekeisti https://www.php.net/manual/en/language.oop5.magic.php
+//Petras
+class Ufo
+{
+
+    // Savybe. Savybes deklaracija
+    public $speed;
+
+    //private $destination = 'Moon';
 
 
-    private function whatTime() : string
+    private $destination;
+    // construct stebuklingas metodas - pasileidzia pats, kai kuriame nauja objekta
+    public function __construct($where, $speed = 5)
     {
-        return date('d:s');
+        $this->destination = $where;
+        $this->speed = $speed;
     }
 
-    public function turbo() : int
+
+    // destruct stebuklingas metodas - pasileidzia tada, kai objektas nustoja egzistuoti atmintyje. Kai baigiasi scriptas, die
+    public function __destruct()
     {
-        return $this->speed * 3;
+        echo '<h1>UFO GONE</h1>';
     }
 
-    public function whatTimeBig() : string
+    // get stebuklingas metodas - leidzia nuskaityti kintamaji
+    public function __get($prop)
+    {
+        return $this->$prop;
+
+        /*
+        if ($prop == 'bebras') {
+            return 'Bebro nėra, bet yra Zebras';
+        }
+        
+        if ($prop == 'destination') {
+            return $this->destination;
+        }
+        */
+
+        //echo "<h1>$prop</h1>";
+    }
+
+    // set stebuklingas metodas - leidzia irasyti i kintamaji. Ka norim ir kur norim irasyti. Su if darom validacija, ka galima irasyti
+    public function __set($prop, $what)
+    {
+        if (in_array($what, ['Moon', 'Mars', 'Pluto'])) {
+            $this->$prop = $what;
+        }
+    }
+
+    /*
+    public function where() : string
+    {
+        return $this->destination;
+    }
+    */
+
+    // Funkcija - metodas
+    private function whatTime(): string // kintamojo tipas - reikia nurodyti, ka grazina metodas. Gali buti string, array, int.... Del klaidu paiesku labai gerai
+    {
+        return date('i:s');
+    }
+
+    public function turbo(): int
+    {
+        return $this->speed * 3; // $this - jei nori kazka pasiekti tame objekte, rasome $this
+    }
+
+
+    public function whatTimeBig(): string
     {
         return '<h1>' . $this->whatTime() . '</h1>';
     }
--- 022/index.php
+++ 022/index.php
@@ -1,32 +1,50 @@
 <?php
+
 //Ona
-
 echo '<pre>';
+// Objektinis programavimas: vienai klasei - vienas failas,  klases vardas is didziosios, failo pavadinimas kaip klases pavadinimas
 
+// Klase pasiimam is failo
 require __DIR__ . '/Ufo.php';
 
-$ufo1 = new Ufo;
-$ufo2 = new Ufo;
+// Kuriame objekta - kintamajame pasigaminame nauja klase
+$ufo1 = new Ufo('Jupiter');
+$ufo2 = new Ufo('Alfa Centauri', 55585658565255);
 
+// unset($ufo2);
 
+// Priskyrimas - perdavimas pagal referensa, nesukuria naujo objekto
+// $ufo3 = $ufo1;
 
+// Objekto kopijavimas - kol kas nenaudosime, advanced funkcija
+// $ufo3 = clone($ufo1);
 
-
-
-$ufo1->speed = 17;
-
-
-// echo $ufo1->speed;
-
-// echo '<br>';
-// echo $ufo1->turbo();
-// echo '<br>';
-// echo $ufo2->turbo();
-
+// Savybe galime pakeisti
+// Kintamojo savybe - nedeti $
+// $ufo1->speed = 17;
 echo '<br>';
-echo  $ufo2->whatTimeBig();
 
+// Isspausdina savybe
+//echo $ufo1->speed;
+//echo '<br>';
+//echo $ufo2->whatTimeBig();
+//echo '<br>';
+//echo $ufo1->turbo();
+//echo '<br>';
+//echo $ufo2->turbo();
+//echo '<br>';
 
+//echo $ufo1->destination;
+//echo '<br>';
+
+//echo $ufo1->destination = 'Mars';
 echo '<br>';
-var_dump($ufo1);
+
+// echo $ufo2->bebras;
+echo '<br>';
+
+//echo $ufo1->where();
+
+// Atspausdina php sukurta objekta
+var_dump($ufo1); 
 var_dump($ufo2);
