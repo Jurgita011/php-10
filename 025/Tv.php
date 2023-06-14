d50784763adb04a79a0e87e162f9fd7a5b2701e0
Jurgita <jurgita011@gmail.com>
2023-06-14 13:14:55 +0300

    new

--- 025/Tv.php
+++ 025/Tv.php
@@ -1,28 +1,30 @@
 <?php
 
 
-class TV {
+class TV
+{
+    // static - turi buti auksciau, pirmas static, nors reiksmes kodavimui neturi. Skaitymui geriau.
 
+    // static savybe - yra klases dalis, neiseina savaime i objekta. Nereikia kurti objekto, galima tiesiai matyti reiksme
     static public $price = '458eur';
-    
+
     public $maker = 'Samsung';
 
-
+    // static metodas/funkcija - iskvieciama klases kontekste
     static public function discount(): void
     {
-        $this->maker = 'Tauras';
+        // $this->maker = 'Tauras'; // nesamone - turedami static dalykus, omeny neturime konkretaus objekto, todel negalime sakyti sitas $this. Kai suplanuojame kazka konkreciai, tuomet $this
         self::$price = rand(50, 200) . 'eur';
     }
 
-
     public function whatPrice(): void
     {
-        echo '<h1>'. self::$price .'</h1>';
+        // norint nuskaityti static reiksme, reikia naudoti self
+        echo '<h1>' . self::$price . '</h1>';
     }
 
     public function changePrice(): void
     {
         self::$price = rand(300, 800) . 'eur';
     }
-
 }
--- 025/index.php
+++ 025/index.php
@@ -1,25 +1,28 @@
 <?php
 
-require __DIR__ . '/TV.php';
+// static - neprieiname prie konkretaus objekto. Naudojama, kai mums reikia kazko bendro tarp visu tos rusies objektu. Pvz. choras ir choristai - static kintamasis - choro pavadinimas, kintamieji - choristai.
 
+require __DIR__ . '/TV.php';
 
 echo '<pre>';
 
+// static savybe -  rasom klase :: ir savybes pavadinima su $
 echo TV::$price;
 
 echo '<br>';
-
 
 $tv1 = new TV;
 $tv2 = new TV;
 
 $tv1->changePrice();
 
-//TV::discount();
-
+// static funkcija - rasom klase :: ir funkcijos pavadinima
+TV::discount();
 
 $tv1->whatPrice();
 $tv2->whatPrice();
 
 var_dump($tv1);
 var_dump($tv2);
+
+echo '<br>';
