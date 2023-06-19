3ef5465bb724f03670a9457616bcc1b27327f99d
Jurgita <jurgita011@gmail.com>
2023-06-19 12:15:23 +0300

    namu darvbai-2versija

---
+++ 027/u7/Krepsys.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Krepsys {
+    const DYDIS = 500;
+    private $svoris = 0;
+
+    public function deti(Grybas $grybas): bool {
+        if ($grybas->valgomas && !$grybas->sukirmijes) {
+            $this->svoris += $grybas->svoris;
+            return self::DYDIS >= $this->svoris;
+        }
+        return true;
+    }
+}
